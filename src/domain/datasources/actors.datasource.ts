daacc9d024ba649fa2fe2f545534228ed575bfd1
Chris-Am <c.aranguizm@outlook.com>
2023-07-26 11:06:27 -0400

    :bug: solving returning error at actors ds

--- src/domain/datasources/actors.datasource.ts
+++ src/domain/datasources/actors.datasource.ts
@@ -1,5 +1,5 @@
 import { ActorEntity } from '../entities/actor.entity';
 
 export interface ActorsDataSource {
-  getActorsByMovieId(movieId: string): Promise<ActorEntity>[];
+  getActorsByMovieId(movieId: string): Promise<ActorEntity[]>;
 }
---
+++ src/infrastructure/datasource/actors.moviedb.datasource.ts
@@ -0,0 +1,27 @@
+import { AxiosService, EnvironmentConfigService } from '../../config/config';
+import { ActorEntity, ActorsDataSource } from 'src/domain/domain';
+import { ActorsResponse, ActorsMovieDBModel } from '../infrastructure';
+
+export class ActorsMovieDBDatasource implements ActorsDataSource {
+  constructor(
+    private readonly connection: AxiosService,
+    private readonly config: EnvironmentConfigService,
+  ) {}
+
+  async getActorsByMovieId(movieId: string): Promise<ActorEntity[]> {
+    const url = `${this.config.getMovieDBUrl()}movie/${movieId}/credits`;
+    const data = await this.connection.get<string>(url);
+    const creditsResponse = ActorsResponse.toActorsMovieDBModel(data);
+
+    const actors: ActorEntity[] = creditsResponse.cast.map((castData) => {
+      const actorEntity = new ActorEntity();
+      actorEntity.id = castData.id;
+      actorEntity.name = castData.name;
+      actorEntity.profilePath = castData.profile_path || '';
+      actorEntity.character = castData.character || '';
+      return actorEntity;
+    });
+
+    return actors;
+  }
+}
